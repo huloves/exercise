6abea2f61cf014d47f168771c5c39b708bef879f
huloves <17791627852@163.com>
2020-10-12 17:58:02 +0800

    add struct queue in qq_number

--- qq_number.c
+++ qq_number.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 
+struct queue
+{
+    char a[100];
+    int head;
+    int tail;
+};
+
+void queue_init(struct queue* queue)
+{
+    // queue->a = {0};
+    for(int i=0; i<100; i++) {
+        queue->a[i] = '\0';
+    }
+    queue->head = 0;
+    queue->tail = 0;
+}
+
+void queue_add(struct queue* queue, char var)
+{
+    queue->a[queue->tail++] = var;
+}
+
+char queue_del(struct queue* queue)
+{
+
+    return queue->a[queue->head++];
+}
+
 int main()
 {
-    char a[100] = {'6', '3', '1', '7', '5', '8', '9', '2', '4'};
-    int head, tail;
-    int temp;
-    char ans[100] = {0};
+    // char a[100] = {'6', '3', '1', '7', '5', '8', '9', '2', '4'};
+    struct queue q;
+    // int head, tail;
+    // int temp;
 
-    head = 0;
-    int i=0;
-    for(tail=0; a[tail+1] != '\0'; tail++);
-    while(a[head] != '\0') {
-        ans[i++] = a[head];
-        a[tail+1] = a[head+1];
-        head += 2;
-        tail += 1;
+    // head = 0;
+    // int i=0;
+    // for(tail=0; a[tail+1] != '\0'; tail++);
+    // while(a[head] != '\0') {
+    //     ans[i++] = a[head];
+    //     a[tail+1] = a[head+1];
+    //     head += 2;
+    //     tail += 1;
+    // }
+    queue_init(&q);
+    char temp;
+    for(int i=0; i<9; i++) {
+        scanf("%c", &temp);
+        queue_add(&q, temp);
     }
 
-    printf("%s\n", ans);
+    while(q.head < q.tail) {
+        printf("%c ", queue_del(&q));
+        temp = queue_del(&q);
+        queue_add(&q, temp);
+    }
+    printf("\n");
+
+    return 0;
 }
