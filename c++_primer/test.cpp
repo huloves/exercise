71088c2622bc497b1f3214368369e45bdaf4fb04
huloves <17791627852@163.com>
2020-12-21 23:29:13 +0800

    test

--- c++_primer/test.cpp
+++ c++_primer/test.cpp
@@ -10,22 +10,12 @@ using std::vector;
 
 int main(int argc, char* argv[])
 {
-    string s1;
-    string s2 = s1;
-    string s3 = "hiya";
-    string s4(10, 'c');
-    string s5 = "hiya";
-    string s6("hiya");
-    string s7(10, 'c');
-    string s8 = string(10, 'c');
-    string str1 = "hello", str2 = "world";
-    string str3 = str1 + ", " + str2 + '\n';
-    
-    string str("hello world!!!");
-    for(decltype(str.size()) index = 0; index != str.size() && !isspace(str[index]); index++) {
-        str[index] = toupper(str[index]);
+    vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(auto &i : v) {
+        i *= i;
     }
-    cout << str << endl;
-
-    return 0;
+    for(auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
 }
