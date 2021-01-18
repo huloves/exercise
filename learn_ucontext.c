cb588f7b56628e8ea051def4648ca7023b44ac2a
huloves <17791627852@163.com>
2021-01-18 17:27:21 +0800

    update

--- learn_ucontext.c
+++ learn_ucontext.c
@@ -4,14 +4,40 @@
 #include <string.h>
 #include <ucontext.h>
 
+void func1(void* arg)
+{
+    printf("1\n");
+    printf("11\n");
+    printf("111\n");
+    printf("1111\n");
+}
+
+void context_test()
+{
+    char stack[1024 * 128];
+    ucontext_t child, main;
+
+    getcontext(&child);
+    child.uc_stack.ss_sp = stack;
+    child.uc_stack.ss_size = sizeof(stack);
+    child.uc_stack.ss_flags = 0;
+    child.uc_link = &main;
+
+    makecontext(&child, (void*)func1, 0);
+    swapcontext(&main, &child);
+    printf("main\n");
+}
+
 int main(int argc, char** argv)
 {
     ucontext_t context;
 
-    getcontext(&context);
-    printf("hello world!\n");
-    sleep(1);
-    setcontext(&context);
+    // getcontext(&context);
+    // printf("hello world!\n");
+    // sleep(1);
+    // setcontext(&context);
+
+    context_test();
 
     return 0;
 }
