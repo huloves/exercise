e7bdf36bd4a6ef31ad33f80632bc9897054b1d12
huloves <17791627852@163.com>
2022-02-20 16:59:51 +0800

    c++

--- c++_primer/ch7/test.cpp
+++ c++_primer/ch7/test.cpp
@@ -2,13 +2,28 @@
 #include <string>
 #include <vector>
 #include "Sales_data.h"
+#include "Person.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Sales_data totle;
-    totle.bookNo = "123";
+    Sales_data totle("123");
+    Sales_data totle2("qwe", 1, 3);
+    Sales_data totle3;
+    // Sales_data totle4(cin);
     cout << totle.isbn() << endl;
+    print(cout, totle);
+    cout << endl;
+    print(cout, totle2);
+    cout << endl;
+    print(cout, totle3);
+    cout << endl;
+    // print(cout, totle4);
+    // cout << endl;
+    Person person("hqw", "huloves");
+    // read(cin, person);
+    print(cout, person);
+    cout << endl;
     return 0;
 }
