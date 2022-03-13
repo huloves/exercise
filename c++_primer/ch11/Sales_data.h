efb9ad9f8103e55bd1b309989f53a1a581185ea4
huloves <17791627852@163.com>
2022-03-13 22:12:49 +0800

    c++

---
+++ c++_primer/ch11/Sales_data.h
@@ -0,0 +1,74 @@
+#ifndef __SALES_DATA_H
+#define __SALES_DATA_H
+
+#include <string>
+#include <iostream>
+
+class Sales_data {
+friend Sales_data add(const Sales_data&, const Sales_data&);
+friend std::istream &read(std::istream&, Sales_data&);
+friend std::ostream &print(std::ostream&, const Sales_data&);
+
+public:
+    //构造函数
+    // Sales_data() = default;   // 默认构造函数，在参数列表后面协商 = default来要求编译器生成构造函数
+    explicit Sales_data(std::string s = "") : bookNo(s) { }
+    // Sales_data(const std::string &s) : bookNo(s) { }   //构造函数初始值列表。花括号定义了空的函数体
+    Sales_data(const std::string &s, unsigned n, double p) :
+                bookNo(s), units_sold(n), revenue(p * n) { }
+    explicit Sales_data(std::istream &is);
+    std::string isbn() const { return this->bookNo; }
+    Sales_data& combine(const Sales_data&);
+private:
+    // 关于Sales_data对象的操作
+    // std::string isbn() const { return bookNo; }
+    inline double avg_price() const
+        { return units_sold ? revenue / units_sold : 0; }
+    // 数据乘员
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+// Sales_data的非成员接口
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+// double Sales_data::avg_price() const {
+//     if(units_sold) {
+//         return revenue / units_sold;
+//     } else {
+//         return 0;
+//     }
+// }
+
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_data::Sales_data(std::istream &is) {
+    read(is, *this);
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+std::istream &read(std::istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item) {
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+#endif
---
+++ c++_primer/ch11/test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+#include <utility>
+#include "Sales_data.h"
+
+using namespace std;
+
+bool compareIsin(const Sales_data &lhs, const Sales_data &rhs) {
+	return lhs.isbn() < rhs.isbn();
+}
+
+int main()
+{
+	map<string , size_t> word_count;   // 空容器
+	set<string> exclude = {"The", "But", "And", "Or", "An", "A", "the", "but", "and", "or", "an", "a"};
+	map<string, string> authors = { {"Joyce", "Jmaes"},
+									{"Austen", "Jame"},
+									{"Dickens", "Charles"} };
+	vector<int> ivec;
+	for(vector<int>::size_type i = 0; i != 10; i++) {
+		ivec.push_back(i);
+		ivec.push_back(i);
+	}
+	set<int> iset(ivec.cbegin(), ivec.cend());
+	multiset<int> miset(ivec.cbegin(), ivec.cend());
+	cout << ivec.size() << endl;
+	cout << iset.size() << endl;
+	cout << miset.size() << endl;
+
+	multiset<Sales_data, decltype(compareIsin)*> bookstore(compareIsin);
+
+	pair<string, string> anon;
+	// pair<string, size_t> word_count;
+	pair<string, vector<int>> line;
+	pair<string, string> author_{"James", "Joyce"};
+
+	string word = "hello";
+	word_count[word]++;
+	word_count["hello"]++;
+	auto map_it = word_count.begin();
+	cout << map_it->first << " " << map_it->second << endl;;
+	// map_it->first = "new key";   // error
+	map_it->second++;
+	cout << map_it->first << " " << map_it->second << endl;
+
+	return 0;
+}
