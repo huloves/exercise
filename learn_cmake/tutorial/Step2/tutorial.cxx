5773f81e49658cc90a9f54383366063d10360293
huloves <17791627852@163.com>
2022-07-08 12:01:05 +0800

    learn_cmake step2 complete

--- learn_cmake/tutorial/Step2/tutorial.cxx
+++ learn_cmake/tutorial/Step2/tutorial.cxx
@@ -4,6 +4,9 @@
 #include <string>
 
 #include "TutorialConfig.h"
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -17,9 +20,14 @@ int main(int argc, char* argv[])
 
   // convert input to double
   const double inputValue = std::stod(argv[1]);
-
+  
   // calculate square root
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
+
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
