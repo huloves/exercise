3c1f9a5a2dcd93fde2d5731a70a0e71802325980
huloves <17791627852@163.com>
2022-07-11 00:56:10 +0800

    cmake Step3 complete

---
+++ learn_cmake/tutorial/Step3_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/huloves/repositories/exercise/learn_cmake/tutorial/Step3/tutorial.cxx" "/home/huloves/repositories/exercise/learn_cmake/tutorial/Step3_build/CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/huloves/repositories/exercise/learn_cmake/tutorial/Step3/MathFunctions/$(CMAKE_CURRENT_SOURCE_DIR)"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/huloves/repositories/exercise/learn_cmake/tutorial/Step3_build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
